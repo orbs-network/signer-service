11393c93ad9cd8bd718a9d488671adef09f0fbee
Kirill Maksimov <kirill@orbs.com>
2020-06-23 23:22:41 +0300

    WIP test for EthSign

--- service/service_test.go
+++ service/service_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/orbs-network/crypto-lib-go/crypto/encoding"
 	"github.com/orbs-network/crypto-lib-go/crypto/ethereum/digest"
 	"github.com/orbs-network/crypto-lib-go/crypto/ethereum/signature"
+	"github.com/orbs-network/crypto-lib-go/crypto/hash"
 	"github.com/orbs-network/crypto-lib-go/test/crypto/ethereum/keys"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/services"
@@ -67,4 +68,36 @@ func Test_RecoverSenderPublicKey(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println(encoding.EncodeHex(digest.CalcNodeAddressFromPublicKey(key)))
+}
+
+func TestService_EthSign(t *testing.T) {
+	cfg := &signerServiceConfig{}
+	pk := cfg.NodePrivateKey()
+
+	//testOutput := log.NewTestOutput(t, log.NewHumanReadableFormatter())
+	//testLogger := log.GetLogger().WithOutput(testOutput)
+
+	//service := NewService(cfg, testLogger)
+
+	//payload := []byte("payload")
+
+	payload, err := encoding.DecodeHex("f83f808b32303030303030303030308532313030309435353535353535353535353535353535353535359331303030303030303030303030303030303030820001")
+	require.NoError(t, err)
+	fmt.Println(payload)
+
+	h := hash.CalcKeccak256(payload)
+	fmt.Println(h, len(h))
+	signed, err := signature.SignEcdsaSecp256K1(pk, h)
+	require.NoError(t, err)
+
+	fmt.Println(signed.String())
+
+	require.EqualValues(t, "50b69b24790fbdf91bd0272fef54f7490fb4f61cb07a91a3d61e6c115a6fe80b76df08f4f3a5763bc721423c89c074fec9af0ed86bf889973a85499c4691cbf201", signed.String())
+
+	//clientSignature, err := service.NodeSign(context.TODO(), (&services.NodeSignInputBuilder{
+	//	Data: payload,
+	//}).Build())
+	//require.NoError(t, err)
+	//
+	//require.EqualValues(t, signed, clientSignature.Signature())
 }
